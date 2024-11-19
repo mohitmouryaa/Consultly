5b6f30864c3ad14c0957ade37c935bc4f0877396
Mohit Mourya <mohitmouryaa143@gmail.com>
2024-11-19 15:21:32 +0530

    CHANGE STLES INTO TAILWIND CSS

--- src/components/Chats/RenderMessage.tsx
+++ src/components/Chats/RenderMessage.tsx
@@ -1,31 +1,54 @@
 import React from "react";
 import { Text, View } from "react-native";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function RenderMessage({ item }: { item: any }) {
   const isMyMessage = item.user === "me";
   return (
-    <View className="flex flex-row items-center justify-between my-1.5">
-      {/* Time on the opposite side */}
-      {isMyMessage ? (
-        <Text className="flex text-xs text-[#888] ml-1.5 self-center">
-          {item.time}
-        </Text>
-      ) : null}
-
-      {/* Message bubble */}
-      <View
-        className={`max-w-[70%] p-2.5 mx-2.5 my-1.5 rounded-lg ${
-          isMyMessage ? "self-end bg-[#f0f0f0]" : "self-start bg-[#e3be81]"
-        }`}>
-        <Text className="text-base">{item.text}</Text>
+    <View
+      className={`max-w-[70%] p-2.5 mx-2.5 rounded-xl my-1.5 ${
+        isMyMessage ? "self-end bg-[#f0f0f0]" : "self-start bg-[#e3be81]"
+      }`}>
+      <Text className="text-base">{item.text}</Text>
+      <View className="flex flex-row items-center justify-between mt-2">
+        <Text className="text-xs text-[#888] mr-1.5">{item.time}</Text>
+        {isMyMessage ? renderStatusIcon(item.status) : null}
       </View>
-
-      {/* Time on the opposite side */}
-      {!isMyMessage ? (
-        <Text className="text-[#888] mr-2.5 self-center text-xs">
-          {item.time}
-        </Text>
-      ) : null}
     </View>
   );
 }
+
+const renderStatusIcon = (status: string) => {
+  switch (status) {
+    case "not_sent":
+      return (
+        <AntDesign
+          name="clockcircleo"
+          size={12}
+          className="ml-1.5 text-[#888]"
+        />
+      ); // Not sent icon
+    case "sent":
+      return (
+        <AntDesign name="check" size={12} className="ml-1.5 text-[#888]" />
+      ); // Sent icon (single check circle)
+    case "delivered":
+      return (
+        <AntDesign
+          name="checkcircleo"
+          size={12}
+          className="ml-1.5 text-[#888]"
+        />
+      ); // Delivered icon (filled double check)
+    case "seen":
+      return (
+        <AntDesign
+          name="checkcircleo"
+          size={12}
+          className="ml-1.5 text-[#87CEEB]"
+        />
+      ); // Seen icon (eye outline)
+    default:
+      return null;
+  }
+};
--- src/screens/home/Chat.tsx
+++ src/screens/home/Chat.tsx
@@ -6,7 +6,6 @@ import {
   View,
   FlatList,
   TextInput,
-  StyleSheet,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSocket } from "../../providers/socketProvider";
@@ -14,7 +13,7 @@ import { CHAT_JOINED, CHAT_LEAVED, tabBarStyle } from "../../constants";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useAppSelector } from "../../../store";
 import { getCurrentTime } from "../../lib/utils";
-import AntDesign from "react-native-vector-icons/AntDesign";
+import RenderMessage from "../../components/Chats/RenderMessage";
 
 export default function Chat() {
   const navigation = useNavigation<StackNavigationProp<any>>();
@@ -23,32 +22,15 @@ export default function Chat() {
   const userId = useAppSelector(state => state.user._id);
   const [messageText, setMessageText] = useState("");
   const [messages, setMessages] = useState([
-    { id: "1", text: "Hello!", user: "other", time: "09:00", status: "sent" },
-    {
-      id: "2",
-      text: "Hi, how are you?",
-      user: "me",
-      time: "09:02",
-      status: "sent",
-    },
-    {
-      id: "3",
-      text: "I am fine, thank you!",
-      user: "other",
-      time: "09:03",
-      status: "sent",
-    },
+    { id: "1", text: "Hello!", user: "other", time: "09:00" },
+    { id: "2", text: "Hi, how are you?", user: "me", time: "09:02" },
+    { id: "3", text: "I am fine, thank you!", user: "other", time: "09:03" },
   ]);
-  const { onlineUsers, socket } = useSocket();
-
-  const isUserOnline = useMemo(() => {
-    return chat.members.some((member: any) => onlineUsers.has(member));
-  }, [onlineUsers, chat.members]);
+  const { socket } = useSocket();
 
   useLayoutEffect(() => {
     // HIDE THE TAB BAR FOR THIS SCREN
     navigation?.getParent()?.setOptions({ tabBarStyle: { display: "none" } });
-    console.log("Chat screen mounted", userId);
     socket?.emit(CHAT_JOINED, { chatId: chat._id, userId });
 
     return () => {
@@ -68,240 +50,39 @@ export default function Chat() {
         text: messageText,
         user: "me", // Assuming 'me' is the current user
         time: getCurrentTime(),
-        //status: "delivered",
-        status: "seen",
       };
       setMessages(prev => [...prev, newMessage]);
       setMessageText(""); // Clear input field after sending
     }
   }, [messageText, messages]);
 
-  const renderMessage = ({ item }) => {
-    const isMyMessage = item.user === "me";
-
-    // Function to render the appropriate AntDesign icon based on the message status
-    const renderStatusIcon = status => {
-      switch (status) {
-        case "not_sent":
-          return (
-            <AntDesign
-              name="clockcircleo"
-              size={12}
-              style={styles.statusIcon}
-            />
-          ); // Not sent icon
-        case "sent":
-          return <AntDesign name="check" size={12} style={styles.statusIcon} />; // Sent icon (single check circle)
-        case "delivered":
-          return (
-            <AntDesign
-              name="checkcircleo"
-              size={12}
-              style={styles.statusIcon}
-            />
-          ); // Delivered icon (filled double check)
-        case "seen":
-          return (
-            <AntDesign
-              name="checkcircleo"
-              size={12}
-              style={[styles.statusIcon, { color: "#87CEEB" }]}
-            />
-          ); // Seen icon (eye outline)
-        default:
-          return null;
-      }
-    };
-    return (
-      <View
-        style={[
-          styles.messageContainer,
-          isMyMessage ? styles.myMessage : styles.otherMessage,
-        ]}>
-        <Text style={styles.messageText}>{item.text}</Text>
-        <View style={styles.timeIconRow}>
-          <Text style={styles.time}>{item.time}</Text>
-          {isMyMessage && renderStatusIcon(item.status)}{" "}
-          {/* Status icon only for my messages */}
-        </View>
-      </View>
-      // <View style={styles.messageRow}>
-      //   {/* Time on the opposite side */}
-      //   {isMyMessage ? <Text style={styles.timeLeft}>{item.time}</Text> : null}
-
-      //   {/* Message bubble */}
-      //   <View
-      //     style={[
-      //       styles.messageContainer,
-      //       isMyMessage ? styles.myMessage : styles.otherMessage,
-      //     ]}>
-      //     <Text style={styles.messageText}>{item.text}</Text>
-      //   </View>
-
-      //   {/* Time on the opposite side */}
-      //   {!isMyMessage ? (
-      //     <Text style={styles.timeRight}>{item.time}</Text>
-      //   ) : null}
-      // </View>
-    );
-  };
-
   return (
     <SafeAreaView className="flex h-full bg-white">
       {/* HEADER */}
-      {/* <View className="flex flex-row justify-between p-2 mt-3">
-        <View className="flex flex-row">
-          <TouchableOpacity className="my-auto" onPress={handlePress}>
-            <Image source={icons.backArrowIcon} className="w-7 h-7" />
-          </TouchableOpacity>
-          <View className="w-3/6 ml-4">
-            <Text
-              className="overflow-hidden text-lg font-JakartaBold text-ellipsis text-nowrap"
-              numberOfLines={1}
-              ellipsizeMode="tail">
-              {user.name}
-            </Text>
-            <Text
-              className="overflow-hidden text-xs font-JakartaLight text-ellipsis text-nowrap"
-              numberOfLines={1}
-              ellipsizeMode="tail">
-              {isUserOnline ? "Available now" : "Offline"}
-            </Text>
-          </View>
-        </View>
-        <View className="flex flex-row items-center justify-between gap-4">
-          <TouchableOpacity>
-            <Feather name="video" size={27} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Entypo
-              name="dots-three-vertical"
-              size={25}
-              color="black"
-              className=""
-            />
-          </TouchableOpacity>
-        </View>
-      </View> */}
-      <View style={styles.container}>
+      <View className="flex flex-1 bg-white">
         {/* Chat Messages */}
         <FlatList
           data={messages}
           keyExtractor={item => item.id}
-          renderItem={renderMessage}
-          // onContentSizeChange={() =>
-          //   flatListRef.current?.scrollToEnd({ animated: true })
-          // }
-          // onLayout={() => flatListRef.current?.scrollToEnd({ animated: true })}
-          // Disable the inverted prop, which could flip the order
+          renderItem={RenderMessage}
           inverted={false}
-          //inverted
-          // Scroll to the bottom when the list changes (new message is added)
         />
 
         {/* Input field to send new messages */}
-        <View style={styles.inputContainer}>
+        <View className="flex flex-row items-center p-2.5 border-t-[1px] bg-[#fff] border-[##ddd]">
           <TextInput
-            style={styles.input}
+            className="flex flex-1 h-10 border-[#ccc] border-[1px] rounded-3xl px-2.5 bg-[#f0f0f0]"
             placeholder="Type a message..."
             value={messageText}
             onChangeText={setMessageText}
           />
-          <TouchableOpacity onPress={sendMessage} style={styles.sendButton}>
-            <Text style={styles.sendButtonText}>Send</Text>
+          <TouchableOpacity
+            onPress={sendMessage}
+            className="ml-2.5 bg-[#FFA001] px-2 py-3.5 rounded-2xl">
+            <Text className="text-base text-[#fff]">Send</Text>
           </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "bg-white",
-  },
-  messageContainer: {
-    maxWidth: "70%",
-    padding: 10,
-    marginHorizontal: 10,
-    borderRadius: 10,
-    marginVertical: 5,
-  },
-  myMessage: {
-    alignSelf: "flex-end",
-    backgroundColor: "#f0f0f0",
-  },
-  otherMessage: {
-    alignSelf: "flex-start",
-    //backgroundColor: "#4ed197",
-    backgroundColor: "#e3be81",
-  },
-  messageText: {
-    fontSize: 16,
-  },
-  messageRow: {
-    flexDirection: "row", // Align message and time in a row
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginVertical: 5,
-  },
-  timeLeft: {
-    fontSize: 12,
-    color: "#888",
-    marginLeft: 5,
-    //alignSelf: "flex-start", // Time for the other user (left)
-    alignSelf: "center",
-  },
-  timeRight: {
-    fontSize: 12,
-    color: "#888",
-    marginRight: 5,
-    //alignSelf: "flex-end", // Time for "me" (right)
-    alignSelf: "center",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 10,
-    borderTopWidth: 1,
-    borderColor: "#ddd",
-    backgroundColor: "#fff",
-  },
-  input: {
-    flex: 1,
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 20,
-    paddingHorizontal: 10,
-    backgroundColor: "#f0f0f0",
-  },
-  sendButton: {
-    marginLeft: 10,
-    //backgroundColor: "#007AFF",
-    backgroundColor: "#FFA001",
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    borderRadius: 20,
-  },
-  sendButtonText: {
-    color: "#fff",
-    fontSize: 16,
-  },
-  timeIconRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 5,
-  },
-  time: {
-    fontSize: 12,
-    color: "#888",
-    marginRight: 5,
-  },
-  statusIcon: {
-    marginLeft: 5,
-    color: "#888", // Default icon color (you can adjust it as needed)
-  },
-});
