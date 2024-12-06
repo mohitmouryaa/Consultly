f280cb66dc5f4d6203adc29e3bb6ae2658b75378
Mohit Mourya <mohitmouryaa143@gmail.com>
2024-12-06 11:34:12 +0530

    IOS BUILD FIX

--- ios/ConsultlyTests/ConsultlyTests.m
+++ ios/ConsultlyTests/ConsultlyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ConsultlyTests : XCTestCase
+@interface consultlyTests : XCTestCase
 
 @end
 
-@implementation ConsultlyTests
+@implementation consultlyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
